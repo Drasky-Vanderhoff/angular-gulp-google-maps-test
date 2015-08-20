5a7c3fa3031ff4562e32e5edea03ee08df2d3e66
Alejandro Kondrasky <drasky.vanderhoff@gmail.com>
2015-08-20 02:54:18 -0300

    Adding about section, still need to be fixed the possition error and it actually needs a general rework.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # angular-gulp-google-maps-test
-A combination of generator-gulp-angular + Google Maps Api using multiple 3rd party angular directives.
+
+A combination of [generator-gulp-angular](https://github.com/Swiip/generator-gulp-angular) + Google Maps Api using multiple 3rd party angular directives.
--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,6 +13,12 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'app/about/about.html',
+        controller: 'AboutController',
+        controllerAs: 'about'
       });
 
     $urlRouterProvider.otherwise('/');
