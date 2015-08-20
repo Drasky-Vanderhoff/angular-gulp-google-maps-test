7f857cb696e837187b8507e3a95265ad3db412d9
Alejandro Kondrasky <drasky.vanderhoff@gmail.com>
2015-08-20 03:02:19 -0300

    Actually adding about now and removing useless angular tooltips.

---
+++ src/app/about/about.controller.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angularGulpTest')
+    .controller('AboutController', AboutController);
+
+  /** @ngInject */
+  function AboutController() {
+  }
+})();
